6482cad134bcb71df102b0be7bc3718e96fbb6ed
Gordon Laiseniasaenile <gleorush24@Gordons-Air.modem>
2026-05-12 19:52:51 +1000

    Use environment variables for API keys

--- config/config.go
+++ config/config.go
@@ -1,13 +1,19 @@
 package config
 
+import "os"
+
 type Config struct {
 	NewsAPIKey string
 	Port       string
 }
 
 func Load() Config {
+	newsAPIKey := os.Getenv("NEWS_API_KEY")
+	if newsAPIKey == "" {
+		newsAPIKey: "ada9eb876f384277acf4445bfafc4c66"
+	}
 	return Config{
-		NewsAPIKey: "ada9eb876f384277acf4445bfafc4c66",
+		NewsAPIKey: newsAPIKey,
 		Port:       ":8080",
 	}
 }
